02bc39a8e510a5c8384de1adb17fade73b8cdc7c
moxak <moxak.b@gmail.com>
2023-08-01 09:55:28 +0900

    感情分析FT

---
+++ datasets/sentiment_classification/dataset_loader.py
@@ -0,0 +1,33 @@
+# https://huggingface.co/docs/datasets/create_dataset
+from datasets import DatasetBuilder, GeneratorBasedBuilder
+import datasets
+import csv
+
+class FTDataset(GeneratorBasedBuilder):
+    def _info(self):
+        return datasets.DatasetInfo(
+            features=datasets.Features({
+                'text': datasets.Value('string'),
+                'label': datasets.ClassLabel(names=['negative', 'positive', 'neutral']),
+            }),
+        )
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+
+        train_path = './train.csv'
+        test_path = './test.csv'
+
+        return [
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": train_path}),
+            datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={"filepath": test_path}),
+        ]
+
+    def _generate_examples(self, filepath):
+        # CSVファイルを行ごとに読み込み、それぞれの行をHugging Faceデータセットの形式に変換
+        with open(filepath, encoding='utf-8') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for id_, row in enumerate(csv_reader):
+                yield id_, {
+                    'text': row[0], 
+                    'label': row[1], 
+                }
